f060618669d138f0719fefd8937731b8d2264a5f
Murtaza Hussain <murtazahussain@Murtazas-MacBook-Pro.local>
2025-11-03 16:12:36 -0500

    Update server index to use database manager from db/index.js

--- server/index.js
+++ server/index.js
@@ -1,34 +1,33 @@
 // THESE ARE NODE APIs WE WISH TO USE
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const cookieParser = require('cookie-parser')
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 
 // CREATE OUR SERVER
-dotenv.config()
+dotenv.config();
 const PORT = process.env.PORT || 4000;
-const app = express()
+const app = express();
 
 // SETUP THE MIDDLEWARE
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: ["http://localhost:3000"],
     credentials: true
-}))
-app.use(express.json())
-app.use(cookieParser())
+}));
+app.use(express.json());
+app.use(cookieParser());
 
 // SETUP OUR OWN ROUTERS AS MIDDLEWARE
-const authRouter = require('./routes/auth-router')
-app.use('/auth', authRouter)
-const storeRouter = require('./routes/store-router')
-app.use('/store', storeRouter)
+const authRouter = require('./routes/auth-router');
+app.use('/auth', authRouter);
+
+const storeRouter = require('./routes/store-router');
+app.use('/store', storeRouter);
 
 // INITIALIZE OUR DATABASE OBJECT
-const db = require('./db')
-db.on('error', console.error.bind(console, 'Database connection error:'))
+// This will load the appropriate DatabaseManager based on DATABASE_TYPE in .env
+const dbManager = require('./db');
 
 // PUT THE SERVER IN LISTENING MODE
-app.listen(PORT, () => console.log(`Playlister Server running on port ${PORT}`))
-
-
+app.listen(PORT, () => console.log(`Playlister Server running on port ${PORT}`));
